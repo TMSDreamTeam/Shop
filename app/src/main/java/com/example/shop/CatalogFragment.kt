b043a2e298773ac4a534fce6ac0e265ea013c737
stasenkots <tstasenko2@gmail.com>
2020-10-09 19:58:44 +0300

    dine CatalogFragment.kt

--- app/src/main/java/com/example/shop/CatalogFragment.kt
+++ app/src/main/java/com/example/shop/CatalogFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.fragment_catalog.*
 
 
 class CatalogFragment : Fragment() {
@@ -25,6 +27,15 @@ class CatalogFragment : Fragment() {
         val descriptions=resources.getStringArray(R.array.product_descriptions)
         for (i in 0..names.size){
             Catalog.catalog.add(Item(names[i],prices[i],urls[i],descriptions[i]))
+        }
+
+        val viewManager = LinearLayoutManager(context)
+        val viewAdapter = RecycleItemAdapter(Catalog.catalog)
+
+        recyclerView_catalog.apply {
+            setHasFixedSize(true)
+            layoutManager = viewManager
+            adapter = viewAdapter
 
         }
     }
