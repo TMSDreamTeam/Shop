c35248c4b94dea2416fb128ea19bdea1e32c9dc4
stasenkots <tstasenko2@gmail.com>
2020-10-09 20:35:56 +0300

    add Catalog.kt and CatalogFragment.kt

--- app/src/main/java/com/example/shop/CatalogFragment.kt
+++ app/src/main/java/com/example/shop/CatalogFragment.kt
@@ -7,14 +7,15 @@ import android.view.View
 import android.view.ViewGroup
 
 
-class DescriptionFragment : Fragment() {
-
+class CatalogFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_description, container, false)
+
+        return inflater.inflate(R.layout.fragment_catalog, container, false)
     }
+
+
 }
