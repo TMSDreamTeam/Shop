3a161c960d1a614202aa8caee19fce35c781258f
stasenkots <tstasenko2@gmail.com>
2020-10-09 20:34:17 +0300

    edit RecycleItemAdapter.kt

--- app/src/main/java/com/example/shop/Catalog.kt
+++ app/src/main/java/com/example/shop/Catalog.kt
@@ -2,4 +2,5 @@ package com.example.shop
 
 object Catalog {
     val catalog = arrayListOf<Item>()
+    lateinit var selectedItem:Item
 }
--- app/src/main/java/com/example/shop/RecycleItemAdapter.kt
+++ app/src/main/java/com/example/shop/RecycleItemAdapter.kt
@@ -8,7 +8,10 @@ import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.item_view.view.*
 
 class RecyclerItemAdapter(private val list: List<Item>) : RecyclerView.Adapter<RecyclerItemAdapter.ItemViewHolder>() {
+    interface ItemClick{
+        fun onItemClick(position: Int)
 
+    }
     class ItemViewHolder(view: View) : RecyclerView.ViewHolder(view)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerItemAdapter.ItemViewHolder {
@@ -25,9 +28,6 @@ class RecyclerItemAdapter(private val list: List<Item>) : RecyclerView.Adapter<R
         Picasso.get().load(list[position].photoUrl).into(holder.itemView.item_image)
         holder.itemView.item_name.text=list[position].name
         holder.itemView.item_price.text=list[position].price
-        holder.itemView.setOnClickListener{
-            descript
-
-        }
+        Catalog.selectedItem=list[position]
     }
 }
